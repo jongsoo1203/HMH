a4e6976549091002e0ad1a4d972bffe1875a917f
WilhenAlbertoHM <wahm0131@gmail.com>
2025-03-01 11:11:37 -0500

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/components/ui/login-form";
 import { ThemeAwareAvatar } from "@/components/theme-aware-avatar";
 
 export default function LoginPage() {
